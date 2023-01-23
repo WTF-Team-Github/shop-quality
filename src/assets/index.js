77f2adcdeeb2ac1e141fa09825319ac060b248cd
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-01-23 19:53:02 +0100

    Add bank building,credit card and pay cash icons Create index file for easy import of icons

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+export const icons = {
+  bankBuilding: require("./icons/bank-building.svg").default,
+  creditCard: require("./icons/credit-card.svg").default,
+  payCash: require("./icons/pay-cash.svg").default,
+};
