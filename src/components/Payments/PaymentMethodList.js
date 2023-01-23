2def2b30d1be85cc5bec76b6492896f724e8f69e
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-01-23 19:53:31 +0100

    Create PaymentMethodList component

---
+++ src/components/Payments/PaymentMethodList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Button from "../../UI/Button";
+import "./PaymentMethodList.css";
+import { FaCreditCard, FaBuilding } from "react-icons/fa";
+import { icons } from "../../assets/index";
+import Card from "../../UI/Card";
+
+const PaymentMethodList = () => {
+  return (
+    <section className="choose-payment">
+      <Card className="choose-payment__card">
+        <div>
+          <img src="" alt="" />
+        <h2 className="choose-payment__heading"> Choose Payment Method</h2>
+
+        </div>
+        <ul className="choose-payment__list">
+          <a className="choose-payment__link" href="">
+            <img src={icons.creditCard} alt="" />
+            <li className="choose-payment__list-item">Card</li>
+          </a>
+
+          <a className="choose-payment__link" href="">
+            <img src={icons.bankBuilding} alt="" />
+            <li className="choose-payment__list-item">USSD Bank Transfer</li>
+          </a>
+
+          <a className="choose-payment__link" href="">
+            <img src={icons.payCash} alt="" />
+            <li className="choose-payment__list-item">Cash on Delivery</li>
+          </a>
+        </ul>
+
+        <Button className="choose-payment__btn"> Add Payment Method</Button>
+      </Card>
+    </section>
+  );
+};
+
+export default PaymentMethodList;
