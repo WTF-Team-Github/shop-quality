53c712f45c14e0a748256345602369420a8e152a
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-01-24 21:42:54 +0100

    Add arrow-left icon Remove reportWebVitals import

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,4 +2,5 @@ export const icons = {
   bankBuilding: require("./icons/bank-building.svg").default,
   creditCard: require("./icons/credit-card.svg").default,
   payCash: require("./icons/pay-cash.svg").default,
+  arrowLeft: require("./icons/arrow-left.svg").default,
 };
