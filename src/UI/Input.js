f7f39b8ad64a4631a034e0392912043b90703ddd
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-01-25 16:24:31 +0100

    Create Input component

---
+++ src/UI/Input.js
@@ -0,0 +1,26 @@
+import "./Input.css";
+import { icons } from "../assets";
+
+const Input = (props) => {
+  const flexLabel = props.icon && "input-label-flex";
+  return (
+    <>
+      <label className={`flexLabel ${props.labelClassName}`} htmlFor={props.id}>
+          <span className="input-label-bold">{props.label}</span>
+          <small className="input-label-small">{props.description}</small>
+
+      </label>
+
+      <input
+        id={props.id}
+        placeholder={props.placeholder}
+        type={props.type}
+        onChange={props.onChange}
+        className={`${props.inputClassName} ${"input"}`}
+        disabled={props.isDisabled}
+      />
+    </>
+  );
+};
+
+export default Input;
