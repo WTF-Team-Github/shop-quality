d0c0e16af9e1b38f0b924872f75f612b82bd94a4
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-01-25 16:22:54 +0100

    Create routing for pages - Add route for payment method list page - Add route for new card page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import PaymentMethodList from "./components/Payments/PaymentMethodList";
-import "./App.css"
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddNewCard from "./components/Cards/AddNewCard";
 
 function App() {
   return (
-    <div>
-      <PaymentMethodList/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<PaymentMethodList />}></Route>
+        <Route path="new-card" element={<AddNewCard />} />
+        {/* <Route path="/cards">
+          <PaymentMethodList />
+        </Route> */}
+        {/* <Route path="/add-card" >
+          <PaymentMethodList />
+        </Route> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
