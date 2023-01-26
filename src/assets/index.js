0aa1e1b61ae349dd771775e1fc5724ad4c299334
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-01-26 14:10:08 +0100

    Add bank-building-white, phone-dial and add icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,10 @@
 export const icons = {
   bankBuilding: require("./icons/bank-building.svg").default,
+  bankBuildingWhite: require("./icons/bank-building-white.svg").default,
   creditCard: require("./icons/credit-card.svg").default,
   payCash: require("./icons/pay-cash.svg").default,
   arrowLeft: require("./icons/arrow-left.svg").default,
+  add: require("./icons/add.svg").default,
+  phoneDial: require("./icons/phone-dial.svg").default,
+
 };
