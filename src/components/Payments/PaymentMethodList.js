07ba9d9f61ab6d140c4fcc6d2bbbda0e657487db
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-01-26 14:15:23 +0100

    Increase spacing between all  elements in paymentMethodList component

--- src/components/Payments/PaymentMethodList.js
+++ src/components/Payments/PaymentMethodList.js
@@ -7,35 +7,52 @@ import { Link } from "react-router-dom";
 
 const PaymentMethodList = () => {
   return (
-    <section className="choose-payment">
-      <Card className="choose-payment__card">
+    <section className="choose-payment ">
+      <Card className="choose-payment__card container">
         <div>
           <img className="choose-payment__icons" src={icons.arrowLeft} alt="" />
           <h2 className="choose-payment__heading"> Choose Payment Method</h2>
         </div>
 
-        <ul className="choose-payment__list">
-          <Link to="/cards" className="choose-payment__link" >
-            <img className="choose-payment__icons" src={icons.creditCard} alt="" />
-            <li className="choose-payment__list-item">Card</li>
-          </Link>
+        <div>
+          <ul className="choose-payment__list">
+            <Link to="/cards" className="choose-payment__link">
+              <img
+                className="choose-payment__icons"
+                src={icons.creditCard}
+                alt=""
+              />
+              <li className="choose-payment__list-item">Card</li>
+            </Link>
 
-          <a className="choose-payment__link" href="">
-            <img className="choose-payment__icons" src={icons.bankBuilding} alt="" />
-            <li className="choose-payment__list-item">USSD Transfer</li>
-          </a>
+            <a className="choose-payment__link" href="">
+              <img
+                className="choose-payment__icons"
+                src={icons.phoneDial}
+                alt=""
+              />
+              <li className="choose-payment__list-item">USSD Transfer</li>
+            </a>
 
-          <a className="choose-payment__link" href="">
-            <img className="choose-payment__icons" src={icons.bankBuilding} alt="" />
-            <li className="choose-payment__list-item">Use Bank App</li>
-          </a>
+            <a className="choose-payment__link" href="">
+              <img
+                className="choose-payment__icons"
+                src={icons.bankBuilding}
+                alt=""
+              />
+              <li className="choose-payment__list-item">Use Bank App</li>
+            </a>
 
-          <a className="choose-payment__link" href="">
-            <img className="choose-payment__icons" src={icons.payCash} alt="" />
-            <li className="choose-payment__list-item">Cash on Delivery</li>
-          </a>
-        </ul>
-
+            <a className="choose-payment__link" href="">
+              <img
+                className="choose-payment__icons"
+                src={icons.payCash}
+                alt=""
+              />
+              <li className="choose-payment__list-item">Cash on Delivery</li>
+            </a>
+          </ul>
+        </div>
         <Button className="choose-payment__btn"> Add Payment Method</Button>
       </Card>
     </section>
