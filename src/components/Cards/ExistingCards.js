63e1d4b93552aa2694269a2ce89af29789daf17b
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-01-30 08:25:18 +0100

    Update ExistingCards component - Use background image - Add checbox

--- src/components/Cards/ExistingCards.js
+++ src/components/Cards/ExistingCards.js
@@ -11,13 +11,14 @@ const ExistingCards = () => {
   const expiryYear = now.getFullYear() + 2;
 
   const expiryMonth = now.getMonth() + 1;
-  console.log(expiryYear);
 
   const [number, setNumber] = useState("5399456790894378");
   const [month, setMonth] = useState(expiryMonth);
   const [year, setYear] = useState(expiryYear);
   const [holder, setHolder] = useState("Deborah Animashaun");
   const [cvv, setCvv] = useState("456");
+  const cardId = number.slice(number.length - 4);
+
   return (
     <section className="cards">
       <Card className="cards__card">
@@ -49,6 +50,14 @@ const ExistingCards = () => {
           hideDigits="true"
           scale="2"
         />
+
+        <div className="select-card__div">
+          <input className="select-card__checkbox" type="checkbox" name="" id={cardId} />
+          <label className="select-card__label" htmlFor={cardId}>
+            Use as default payment method
+          </label>
+        </div>
+
         <Link to={"/new-card"}>
           <Button className="add-card__btn"> Add New Card</Button>
         </Link>
