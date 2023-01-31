109449f312982d185153afe31ff797cca1eec1ce
Adeola Adegbusi <adeadeola234@gmail.com>
2023-01-31 23:25:48 +0100

    Login component linked to signup component

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import leftIcon from "../assets/left-icon.png";
 import "./Login.css";
-
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -13,37 +13,53 @@ function Login() {
   };
 
   return (
-    <div className="loginWrapper">
-      <div className="auth-form-container">
-        <h1>Login</h1>
-        <form className="loginForm" onSubmit={handleSubmit}>
-          <label htmlFor="email">Email </label>
-          <input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="email"
-            placeholder="youremail@abc.xyz"
-            id="email"
-            name="email"
-          />
-          <label htmlFor="password"> Password </label>
-          <input
-            value={pass}
-            onChange={(e) => setPass(e.target.value)}
-            type="password"
-            placeholder="*************"
-            name="password"
-            id="password"
-          />
-          <button className='submitBtn' type="submit">Login</button>
-        </form>
-        
-        <Link to='/Signup'>
-        <button className='linkBtn' >Don't have an account? Register here</button>
-        
-        </Link>
+    <>
+      <Link to="./Signup">
+        <img
+          src={leftIcon}
+          alt="left-pointing-arrow"
+          width="30px"
+          height="30px"
+          className="leftIcon"
+        />
+      </Link>
+
+      <div className="loginWrapper">
+        <div className="auth-form-container">
+          <h1>Login</h1>
+          <form className="loginForm" onSubmit={handleSubmit}>
+            <label htmlFor="email">Email </label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="youremail@abc.xyz"
+              id="email"
+              name="email"
+            />
+            <label htmlFor="password"> Password </label>
+            <input
+              value={pass}
+              onChange={(e) => setPass(e.target.value)}
+              type="password"
+              placeholder="*************"
+              name="password"
+              id="password"
+            />
+            <button className="submitBtn" type="submit">
+              Login
+            </button>
+          </form>
+
+          <Link to="/Signup">
+            <button className="linkBtn">
+              {" "}
+              Don't have an account? Register here{" "}
+            </button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
