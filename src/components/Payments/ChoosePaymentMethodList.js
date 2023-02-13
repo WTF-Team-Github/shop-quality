e75196b5643148e89e864973ae0c62358d770c6d
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-02-13 11:12:22 +0100

    Update ChoosePaymentMethodList component - Reduce font-sizes and spacing - Add radio button to help user select payment method - Disable btn till a payment method is selected

--- src/components/Payments/ChoosePaymentMethodList.js
+++ src/components/Payments/ChoosePaymentMethodList.js
@@ -13,7 +13,7 @@ const ChoosePaymentMethodList = () => {
   const [showUSSDModal, setShowUSSDModal] = useState(false);
   const [showTransferModal, setShowTransferModal] = useState(false);
   const [showCashModal, setShowCashModal] = useState(false);
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
   function openModal(e) {
     e.preventDefault();
@@ -29,9 +29,15 @@ const ChoosePaymentMethodList = () => {
     setShowCashModal(false);
   }
 
+  function radioChangeHandler(e) {
+    // console.log(e.current.value);
+    // console.log(e.current.name);
+    console.log(e.target.id);
+  }
+
   return (
     <section className="choose-payment ">
-      <Card className="choose-payment__card container">
+      <Card className="choose-payment__card ">
         <div>
           <img className="choose-payment__icons" src={icons.arrowLeft} alt="" />
           <h2 className="choose-payment__heading"> Choose Payment Method</h2>
@@ -39,65 +45,98 @@ const ChoosePaymentMethodList = () => {
 
         <div>
           <ul className="choose-payment__list">
-            <Link to="/cards" className="choose-payment__link">
-              <img
-                className="choose-payment__icons"
-                src={icons.creditCard}
-                alt=""
+            <div className="choose-payment__div">
+              <Link to="/cards" className="choose-payment__link">
+                <img
+                  className="choose-payment__icons"
+                  src={icons.creditCard}
+                  alt=""
+                />
+                <li className="choose-payment__list-item">Card</li>
+              </Link>
+              <input
+                onChange={radioChangeHandler}
+                type="radio"
+                name="payment-method"
+                id="card"
               />
-              <li className="choose-payment__list-item">Card</li>
-            </Link>
+            </div>
 
-            <a
-              id="USSD"
-              onClick={openModal}
-              className="choose-payment__link"
-              href=""
-            >
-              <img
-                className="choose-payment__icons"
-                src={icons.phoneDial}
-                alt=""
+            <div className="choose-payment__div">
+              <a
+                id="USSD"
+                onClick={openModal}
+                className="choose-payment__link"
+                href=""
+              >
+                <img
+                  className="choose-payment__icons"
+                  src={icons.phoneDial}
+                  alt=""
+                />
+                <li className="choose-payment__list-item">USSD Transfer</li>
+              </a>
+              <input
+                onChange={radioChangeHandler}
+                type="radio"
+                name="payment-method"
+                id="ussd"
               />
-              <li className="choose-payment__list-item">USSD Transfer</li>
-            </a>
-            {showUSSDModal && <PayWithUSSD closeModal={closeModal} />}
 
-            <a
-              id="transfer"
-              onClick={openModal}
-              className="choose-payment__link"
-              href=""
-            >
-              <img
-                className="choose-payment__icons"
-                src={icons.bankBuilding}
-                alt=""
-              />
-              <li className="choose-payment__list-item">Use Bank App</li>
-            </a>
-            {showTransferModal && (
-              <PayWithBankTransfer closeModal={closeModal} />
-            )}
+              {showUSSDModal && <PayWithUSSD closeModal={closeModal} />}
+            </div>
 
-            <a
-              id="cash"
-              onClick={openModal}
-              className="choose-payment__link"
-              href=""
-            >
-              <img
-                className="choose-payment__icons"
-                src={icons.payCash}
-                alt=""
+            <div className="choose-payment__div">
+              <a
+                id="transfer"
+                onClick={openModal}
+                className="choose-payment__link"
+                href=""
+              >
+                <img
+                  className="choose-payment__icons"
+                  src={icons.bankBuilding}
+                  alt=""
+                />
+                <li className="choose-payment__list-item">Use Bank App</li>
+              </a>
+              <input
+                onChange={radioChangeHandler}
+                type="radio"
+                name="payment-method"
+                id="bank-app"
               />
-              <li className="choose-payment__list-item">Cash on Delivery</li>
-            </a>
-            {showCashModal && <PayWithCash closeModal={closeModal} />}
+              {showTransferModal && (
+                <PayWithBankTransfer closeModal={closeModal} />
+              )}
+            </div>
+
+            <div className="choose-payment__div">
+              <a
+                id="cash"
+                onClick={openModal}
+                className="choose-payment__link"
+                href=""
+              >
+                <img
+                  className="choose-payment__icons"
+                  src={icons.payCash}
+                  alt=""
+                />
+                <li className="choose-payment__list-item">Cash on Delivery</li>
+              </a>
+              <input
+                onChange={radioChangeHandler}
+                type="radio"
+                name="payment-method"
+                id="cash"
+              />
+
+              {showCashModal && <PayWithCash closeModal={closeModal} />}
+            </div>
           </ul>
         </div>
-        <Button isDisabled={isButtonDisabled} className="choose-payment__btn">
-          {" "}
+        <Button isDisabled={isButtonDisabled} className="choose-payment__btn ">
           Add Payment Method
         </Button>
       </Card>
